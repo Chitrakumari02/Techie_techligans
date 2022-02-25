ca3326b923392be134ef8cf2aee850e6032e7153
Chitrakumari02 <89096456+Chitrakumari02@users.noreply.github.com>
2022-02-25 21:01:18 +0530

    Create question9.dart

---
+++ task1/question9.dart
@@ -0,0 +1,11 @@
+void main() 
+{
+
+ dynamic ch;
+  
+  //ask input the character
+ 
+  (ch>='a'&& ch<='z')||(ch>='a'&& ch<='z')?
+    print("it is an alphabet"):
+  print("it is not an alphabet");
+}
