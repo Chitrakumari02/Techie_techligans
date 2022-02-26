e8090215adaafea914196870ca99234c92cd005f
Chitrakumari02 <89096456+Chitrakumari02@users.noreply.github.com>
2022-02-26 21:58:08 +0530

    Create question1.dart

---
+++ task2/question1.dart
@@ -0,0 +1,9 @@
+i)ans= int multiply(a,b)=>a*b;
+ii)ans= Async function
+iii)ans= List
+iv)ans= None
+ v)ans= print('Your name is ${name}');
+vi)ans= nums.forEach(print);
+vii)ans= Ensures type safety
+         Reduce code duplication
+         
