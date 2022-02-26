8ab1c27f1c0c3a322e0ba092c5dd40a99d4c4a6c
Chitrakumari02 <89096456+Chitrakumari02@users.noreply.github.com>
2022-02-26 22:00:51 +0530

    Create question4.dart

---
+++ task2/question4.dart
@@ -0,0 +1,32 @@
+void main()
+{
+  Map<dynamic, dynamic> alpha = Map();
+  alpha["1"] = "a";
+  alpha["2"] = "b";
+  alpha["3"] = "c";
+  alpha["4"] = "d";
+  alpha["5"] = "e";
+  alpha["6"] = "f";
+  alpha["7"] = "g";
+  alpha["8"] = "h";
+  alpha["9"] = "i";
+  alpha["10"] = "j";
+  alpha["11"] = "k";
+  alpha["12"] = "l";
+  alpha["13"] = "m";
+  alpha["14"] = "n";
+  alpha["15"] = "o";
+  alpha["16"] = "p";
+  alpha["17"] = "q";
+  alpha["18"] = "r";
+  alpha["19"] = "s";
+  alpha["20"] = "t";
+  alpha["21"] = "u";
+  alpha["22"] = "v";
+  alpha["23"] = "w";
+  alpha["24"] = "x";
+  alpha["25"] = "y";
+  alpha["26"] = "z";
+  print("the asked alphabet according to number is");
+  print(alpha["19"]);
+}
